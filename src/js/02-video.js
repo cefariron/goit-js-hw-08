bc9eff5f2254d6ef915069e969484533622223c0
cefariron <adidaswww17@gmail.com>
2023-11-20 15:31:24 +0200

    complete task 03

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,22 @@
-
 import Player from '@vimeo/player';
 
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+function handleTimeUpdate(event) {
+  const currentTime = event.seconds;
+
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}
+
+const throttledHandleTimeUpdate = throttle(handleTimeUpdate, 1000);
+
+player.on('timeupdate', throttledHandleTimeUpdate);
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+
+  if (savedTime) {
+    player.setCurrentTime(parseFloat(savedTime));
+  }
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+const form = document.querySelector('.feedback-form');
 
+form.addEventListener('input', handleInput);
+
+function handleInput(event) {
+  event.preventDefault();
+
+  const { email, message } = event.currentTarget.elements;
+
+  const currentInfo = {
+    email: email.value,
+    message: message.value,
+  };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(currentInfo));
+}
+
+const curJsonLocalStorageData = localStorage.getItem('feedback-form-state');
+currentLocalStorageData = JSON.parse(curJsonLocalStorageData);
+
+document.addEventListener('DOMContentLoaded', getInfo);
+
+function getInfo(event) {
+  inputEl = document.querySelector('input');
+  textareaEl = document.querySelector('textarea');
+
+  if (curJsonLocalStorageData) {
+    inputEl.value = currentLocalStorageData.email;
+    textareaEl.value = currentLocalStorageData.message;
+  }
+}
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  console.log(currentLocalStorageData);
+  form.reset();
+  localStorage.clear();
+}
