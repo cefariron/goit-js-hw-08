e63fab657e360ff1036fb9bc951548324af0b0f2
cefariron <adidaswww17@gmail.com>
2023-11-20 10:59:03 +0200

    complete task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,61 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items';
+
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+const listEl = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    ({ description, original, preview }) =>
+      `<li class="gallery__item">
+    <a class="gallery__link" href="${original}" target="_blank">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>
+`
+  )
+  .join("");
+
+listEl.insertAdjacentHTML("beforeend", markup);
+
+listEl.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `
+    <div class="modal">
+    <img src="${event.target.dataset.source}" width="800" height="600">
+    </div>
+`,
+    {
+      onShow: () => {
+        document.addEventListener("keydown", handleEsc);
+      },
+      onClose: () => {
+        document.removeEventListener("keydown", handleEsc);
+      },
+    }
+  );
+
+  instance.show();
+
+  function handleEsc(event) {
+    if (event.key === "Escape") {
+      instance.close();
+      return;
+    }
+  }
+});
